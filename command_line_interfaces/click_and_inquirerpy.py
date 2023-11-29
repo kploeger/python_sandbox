c936b005aaabddb3d0976ae60f73d9305a42ccd8
Kai Ploeger <mail@kaiploeger.net>
2023-11-29 23:38:56 +0100

    added click and inquirerpy example

---
+++ command_line_interfaces/click_and_inquirerpy.py
@@ -0,0 +1,51 @@
+"""
+    mail@kaiploeger.net
+"""
+
+import click               # for command line arguments
+import InquirerPy as iq    # for interactive menus
+
+
+# Click is similar to argparse, but is more aimed to write whole applications with different commands
+# and takes care of the control flow while arparse intends to "pass arguments to a program".
+
+
+@click.group()
+def app():
+   """Welcome to this test of click and InquirerPy!"""
+
+
+@app.command(name='hello')
+@click.argument('name', type=click.STRING)
+@click.option('--greeting', '-g', type=click.STRING, default='Hello')
+def run(name, greeting):
+    """Say hello to NAME"""
+    click.echo(f'{greeting} {name}!')
+
+
+@app.command(name='ask')
+def this_does_not_need_to_be_named_ask():
+    """Ask a question"""
+
+    feeling_prompt = {
+        'type': 'list',
+        'name': 'feeling',
+        'message': 'How are you?',
+        'choices': ['good', 'meh', 'bad'],
+    }
+    answers = iq.prompt(feeling_prompt)
+
+    food_prompt = {
+        'type': 'checkbox',
+        'name': 'food',
+        'message': 'What do you want for breakfast?',
+        'choices': ['eggs', 'french toast', 'orange juice', 'pancackes'],
+    }
+    answers = answers.update(iq.prompt(food_prompt))
+
+    click.echo(f'You are {answers["feeling"]} and would like to eat {answers["food"]}')
+
+
+
+if __name__ == '__main__':
+    app()
